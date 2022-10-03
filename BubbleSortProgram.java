8a087ff3f500c57c1a6d599374faaf00a34f8414
gouthamKKr <56692058+gouthamKKr@users.noreply.github.com>
2022-10-03 21:08:54 +0530

    Update BubbleSortProgram.java

--- BubbleSortProgram.java
+++ BubbleSortProgram.java
@@ -3,17 +3,15 @@ public class BubbleSortExampleJava {
         int n = arr.length;  
         int temp = 0;  
          for(int i=0; i < n; i++){  
-                 for(int j=1; j < (n-i); j++){  
+                 for(int j=1;j<(n-i);j++){  
                           if(arr[j-1] > arr[j]){  
                                  //swap elements  
                                  temp = arr[j-1];  
                                  arr[j-1] = arr[j];  
                                  arr[j] = temp;  
                          }  
-                          
                  }  
-         }  
-  
+         }
     }  
     public static void main(String[] args) {  
                 int arr[] ={3,60,35,2,45,320,5};  
